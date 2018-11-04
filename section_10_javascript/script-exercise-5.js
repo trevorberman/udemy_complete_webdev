46077c12b2144c9e7d390d3f64139291cf9c677c
trevorberman <trevorberman@gmail.com>
2018-11-04 15:52:01 -0600

    Part 2 and bonus (run two different ways) tests valid and complete.

--- section_10_javascript/script-exercise-5.js
+++ section_10_javascript/script-exercise-5.js
@@ -19,6 +19,7 @@ if (Number(age) < 18) {
 // 1. Make the above code have a function called checkDriverAge(). Whenever you call this function, you will get prompted for age. Use Function Declaration to create this function.
 // Notice the benefit in having checkDriverAge() instead of copying and pasting the function everytime?
 
+/*
 var age
 
 function checkDriverAge () {
@@ -50,7 +51,29 @@ function ignition () {
 }
 
 ignition()
+*/
 
 // 2. Create another function that does the same thing, called checkDriverAge2() using Function Expression.
 
 // BONUS: Instead of using the prompt. Now, only use the return keyword and make the checkDriverAge() function accept an argument of age, so that if you enter checkDriverAge(92) it returns "Powering On. Enjoy the ride!"
+
+var age
+
+var checkDriverAge2 = function (age) {
+  return age
+}
+
+function ignition () {
+//  checkDriverAge2(92)
+
+  if (age < 18) {
+    alert("Sorry, you're too young to drive this car. Powering off.")
+  } else if (age === 18) {
+    alert('Congratulations on your first year of driving. Enjoy the ride!')
+  } else {
+    alert('Powering On. Enjoy the ride!')
+  }
+}
+
+// ignition()
+ignition(checkDriverAge2(92))
