7fbe3bc388a0f39743d4d24c92d1e03302f13152
trevorberman <trevorberman@gmail.com>
2018-11-05 19:37:50 -0600

    Complete excercise.

--- section_10_javascript/script-exercise-6.js
+++ section_10_javascript/script-exercise-6.js
@@ -1,22 +1,28 @@
 // The Complete Web Developer Udemy Course - Section 10: Javascript Exercise 6
 
 // using this array,
-// var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+var array = ['Banana', 'Apples', 'Oranges', 'Blueberries']
 
 // 1. Remove the Banana from the array.
+array.shift()
 
 // 2. Sort the array in order.
+array.sort()
 
 // 3. Put "Kiwi" at the end of the array.
+array.push('Kiwi')
 
 // 4. Remove "Apples" from the array.
+array.shift()
 
 // 5. Sort the array in reverse order. (Not alphabetical, but reverse
 // the current Array i.e. ['a', 'c', 'b'] becomes ['b', 'c', 'a'])
+array.reverse()
 
 // you should have at the end:
 // ["Kiwi", "Oranges", "Blueberries"]
 
 // using this array,
-// var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+var array2 = ['Banana', ['Apples', ['Oranges'], 'Blueberries']]
 // access "Oranges".
+alert(array2[1][1][0])
