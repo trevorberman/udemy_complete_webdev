52c587a80a21f8f79a4c23f3fe7290f43188faf6
trevorberman <trevorberman@gmail.com>
2018-11-06 08:43:37 -0600

    Add exercise 7fb instructions and base code from last exercise.

---
+++ section_10_javascript/script-exercise-7fb.js
@@ -0,0 +1,28 @@
+// The Complete Web Developer Udemy Course - Section 10: Javascript Exercise 7fb
+
+// Create a basic facebook sign-up and sign-in exercise.
+
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+var user = {
+  username: 'Neil',
+  password: 'IZEKfP*2b4bP'
+}
+
+// 2. Create an array which contains the object you have made above and name the array "database".
+var database = [user]
+
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+var newsfeed = [
+  {
+    username: '',
+    timeline: ''
+  },
+  {
+    username: '',
+    timeline: ''
+  },
+  {
+    username: '',
+    timeline: ''
+  }
+]
