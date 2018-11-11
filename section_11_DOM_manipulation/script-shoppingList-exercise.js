36019c5b5d616243c2b28641b3535127849a4485
trevorberman <trevorberman@gmail.com>
2018-11-10 18:17:16 -0600

    Complete shopping list exercise.

--- section_11_DOM_manipulation/script-shoppingList-exercise.js
+++ section_11_DOM_manipulation/script-shoppingList-exercise.js
@@ -1,1 +1,46 @@
 // The Complete Web Developer Udemy Course - Section 11: Javascript Exercise Shopping List
+
+var input = document.getElementById('userInput')
+var button = document.getElementById('submit')
+var shoppingList = document.querySelector('ul')
+
+function inputLength () {
+  return input.value.length
+}
+
+function addListItem () {
+  var listItem = document.createElement('li')
+  listItem.appendChild(document.createTextNode(input.value))
+  shoppingList.appendChild(listItem)
+  input.value = ''
+}
+
+/*
+button.addEventListener('click', function () {
+  if (inputLength() > 0) {
+    addListItem()
+  }
+})
+
+input.addEventListener('keypress', function (event) {
+  if (inputLength() > 0 && event.keyCode === 13) {
+    addListItem()
+  }
+})
+*/
+
+// Refactor the functions attached to button and input above
+function addListItemAfterClick () {
+  if (inputLength() > 0) {
+    addListItem()
+  }
+}
+
+function addListItemAfterSubmit (event) {
+  if (inputLength() > 0 && event.keyCode === 13) {
+    addListItem()
+  }
+}
+
+button.addEventListener('click', addListItemAfterClick)
+input.addEventListener('keypress', addListItemAfterSubmit)
