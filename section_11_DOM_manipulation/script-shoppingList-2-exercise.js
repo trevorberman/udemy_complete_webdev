f419197471b392f0fa170fd88241a2ea8cd6db1d
trevorberman <trevorberman@gmail.com>
2018-11-13 17:57:23 -0600

    Complete coding challenges as detailed in the script.:sparkles::art:

--- section_11_DOM_manipulation/script-shoppingList-2-exercise.js
+++ section_11_DOM_manipulation/script-shoppingList-2-exercise.js
@@ -4,17 +4,32 @@ var input = document.getElementById('userInput')
 var button = document.getElementById('submit')
 var shoppingList = document.querySelector('ul')
 var items = document.querySelectorAll('li')
+var removalButtons = document.getElementsByClassName('remove-item')
 
 function inputLength () {
   return input.value.length
 }
 
 function addListItem () {
+  // Create and append list item
   var listItem = document.createElement('li')
   listItem.appendChild(document.createTextNode(input.value))
   shoppingList.appendChild(listItem)
+  // Create and append list item removal button
+  var removeButton = document.createElement('span')
+  removeButton.appendChild(document.createTextNode('x'))
+  removeButton.classList.add('remove-item')
+  listItem.appendChild(removeButton)
+  removeButton.addEventListener('click', removeListItem)
+  // Listen to mark list item done
   listItem.addEventListener('click', toggleDone)
+  // Clear text input field
   input.value = ''
+}
+
+function removeListItem () {
+  var li = this.closest('li')
+  li.remove()
 }
 
 /*
@@ -51,13 +66,19 @@ function toggleDone () {
   this.classList.toggle('done')
 }
 
+// Add listeners to existing list items
 for (var i = 0; i < items.length; i++) {
   items[i].addEventListener('click', toggleDone)
+  removalButtons[i].addEventListener('click', removeListItem)
 }
 
 // Exercise: DOM Events - Challenges
 // [x] 1. If you click on the list item, it toggles the .done class on and off.
 
-// [] 2. Add buttons next to each list item to delete the item when clicked on its corresponding delete button.
+// [x] 2. Add buttons next to each list item to delete the item when clicked on its corresponding delete button.
+// [x] Append <span class=remove-item>x</span> to listItems
+// [x] Add styles for .remove-item [text, margins, padding, borders]
+// [x] Add JS to append span and class='remove-item' to list item when new list item is added
+// [x] Add JS to remove list item and span.remove-item when .remove-item button clicked (+ event listener on .remove-item)
 
-// [] 3. BONUS: When adding a new list item, it automatically adds the delete button next to it.
+// [x] 3. BONUS: When adding a new list item, it automatically adds the delete button next to it.
